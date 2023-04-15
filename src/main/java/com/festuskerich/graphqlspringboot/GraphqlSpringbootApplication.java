32a44f7f0498e7310c5223aa3019d8bdf72a7553
festus.kerich <festus.kerich@cellulant.io>
2023-04-15 11:43:49 +0300

    first commit

---
+++ src/main/java/com/festuskerich/graphqlspringboot/GraphqlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.festuskerich.graphqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/festuskerich/graphqlspringboot/GraphqlSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festuskerich.graphqlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
